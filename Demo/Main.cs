a030c9716e8fb66c73e608bc7838845abc3f60a2
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2010-05-04 20:49:24 +0700

    Начальный коммит

---
+++ Demo/Main.cs
@@ -0,0 +1,13 @@
+using System;
+using Core;
+
+namespace Demo
+{
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
