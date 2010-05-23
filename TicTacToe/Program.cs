60ff0322e3afab793a8911c38c65da9cd141db23
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2010-05-24 02:25:47 +0700

    Крестики-Нолики со счетом на двух игроков

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+
+using Conwid.Core;
+using Conwid.Core.Messages;
+using Conwid.Core.Widgets;
+
+namespace TicTacToe
+{
+    using WidgetGroup = UIManager<Widget>;
+    using Color = Conwid.Core.Color;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var theLoop = MessageLoop.Instance;
+            
+            var wgExtra = new WidgetGroup(theLoop.WidgetManager, new Rectangle(1,1,21,8), "Score & Settings");
+            var wgField = new WidgetGroup(theLoop.WidgetManager, new Rectangle(23,1,30,14), "Tic-Tac-Toe game");
+            
+            // widgets
+            var lePlayerName1 = new LineEdit(wgExtra,
+                wgExtra.ClientArea.Location+new Size(1,1),
+                wgExtra.ClientArea.Width/2-1,
+                "Player");
+            var lePlayerName2 = new LineEdit(wgExtra,
+                wgExtra.ClientArea.Location+new Size(1+wgExtra.ClientArea.Width/2,1),
+                wgExtra.ClientArea.Width/2-1,
+                "Bot");
+            var cbIsBot1 = new CheckBox(wgExtra,
+                wgExtra.ClientArea.Location + new Size(2, 3),
+                "bot",
+                width: wgExtra.ClientArea.Width/2-1,
+                state: false);
+            var cbIsBot2 = new CheckBox(wgExtra,
+                wgExtra.ClientArea.Location + new Size(2+wgExtra.ClientArea.Width/2, 3),
+                "bot",
+                width: wgExtra.ClientArea.Width/2-1,
+                state: true);
+            var playerScore1 = new Label(wgExtra,
+                wgExtra.ClientArea.Location + new Size(1, 5),
+                "0",
+                width: wgExtra.ClientArea.Width/2-1,
+                centered: true);
+            var playerScore2 = new Label(wgExtra,
+                wgExtra.ClientArea.Location + new Size(1+wgExtra.ClientArea.Width/2, 5),
+                "0",
+                width:  wgExtra.ClientArea.Width/2-1,
+                centered: true);
+            
+            var gameField = new TicTacToeField(wgField,
+                wgField.ClientArea.Location + new Size(1, 1));
+            
+            var playerStatus1 = new Label(wgField,
+                wgField.ClientArea.Location + new Size(15, 2),
+                "->");
+            var playerStatus2 = new Label(wgField,
+                wgField.ClientArea.Location + new Size(15, 4),
+                "  ");
+            var playerStatusName1 = new Label(wgField,
+                wgField.ClientArea.Location + new Size(18, 2),
+                lePlayerName1.Text,
+                width: lePlayerName1.Area.Width,
+                color: new Color(ConsoleColor.DarkRed, ConsoleColor.Black));
+            var playerStatusName2 = new Label(wgField,
+                wgField.ClientArea.Location + new Size(18, 4),
+                lePlayerName2.Text,
+                width: lePlayerName2.Area.Width,
+                color: new Color(ConsoleColor.DarkBlue, ConsoleColor.Black));
+            var winnerName = new Label(wgField,
+                wgField.ClientArea.Location + new Size(16, 7),
+                "",
+                width: lePlayerName1.Area.Width,
+                color: new Color(ConsoleColor.Yellow, ConsoleColor.Black));
+            var winnerMsg = new Label(wgField,
+                wgField.ClientArea.Location + new Size(16, 8),
+                "",
+                width: winnerName.Area.Width,
+                color: new Color(ConsoleColor.Yellow, ConsoleColor.Black));
+
+            // connections
+            lePlayerName1.OnTextChanged += (
+                (_, text, __) => playerStatusName1.Text = text
+            );
+            lePlayerName2.OnTextChanged += (
+                (_, text, __) => playerStatusName2.Text = text
+            );
+
+            var firstTurnChange = true;
+            gameField.OnTurnChanged += (
+                (_, turn, __) => {
+                    var cross = (turn == TicTacToeField.Type.Cross);
+
+                    playerStatus1.Text = cross ? "->" : "  ";
+                    playerStatus2.Text = cross ? "  " : "->";
+
+                    if( !firstTurnChange )
+                    {
+                        winnerName.Text = "";
+                        winnerMsg.Text = "";
+                    }
+                    else
+                    {
+                        firstTurnChange = false;
+                    }
+                }
+            );
+            gameField.OnGameOver += (
+                (_, winner) => {
+                    var cross = (winner == TicTacToeField.Type.Cross);
+                    
+                    playerStatus1.Text = cross ? "+1" : "  ";
+                    playerStatus2.Text = cross ? "  " : "+1";
+
+                    winnerName.Text = cross ? lePlayerName1.Text : lePlayerName2.Text;
+                    winnerMsg.Text = "rocks!";
+                    firstTurnChange = true;
+
+                    if( cross )
+                        playerScore1.Text = (1+int.Parse(playerScore1.Text)).ToString();
+                    else
+                        playerScore2.Text = (1+int.Parse(playerScore2.Text)).ToString();
+                }
+            );
+
+            Console.CursorVisible = false;
+
+            theLoop.Run();
+        }
+    }
+}
---
+++ TicTacToe/TicTacToeField.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Drawing;
+
+using Conwid.Core;
+using Conwid.Core.Messages;
+
+namespace TicTacToe
+{
+    using Color = Conwid.Core.Color;
+
+    class TicTacToeField : Widget
+    {
+        #region Constants
+        
+        readonly ConsoleKeyInfo GoLeftKeyInfo  = new ConsoleKeyInfo('_', ConsoleKey.LeftArrow, control: false, shift: false, alt: false);
+        readonly ConsoleKeyInfo GoRightKeyInfo  = new ConsoleKeyInfo('_', ConsoleKey.RightArrow, control: false, shift: false, alt: false);
+        readonly ConsoleKeyInfo GoUpKeyInfo  = new ConsoleKeyInfo('_', ConsoleKey.UpArrow, control: false, shift: false, alt: false);
+        readonly ConsoleKeyInfo GoDownKeyInfo  = new ConsoleKeyInfo('_', ConsoleKey.DownArrow, control: false, shift: false, alt: false);
+        readonly ConsoleKeyInfo PutMarkKeyInfo = new ConsoleKeyInfo('_', ConsoleKey.Spacebar, control: false, shift: false, alt: false);
+        
+        readonly Color ActiveCrossColor = new Color()
+        {
+            Foreground = ConsoleColor.Red,
+            Background = ConsoleColor.DarkRed
+        };
+        readonly Color InactiveCrossColor = new Color()
+        {
+            Foreground = ConsoleColor.DarkRed,
+            Background = ConsoleColor.DarkRed
+        };
+        readonly Color ActiveNoughtColor = new Color()
+        {
+            Foreground = ConsoleColor.Blue,
+            Background = ConsoleColor.DarkBlue
+        };
+        readonly Color InactiveNoughtColor = new Color()
+        {
+            Foreground = ConsoleColor.DarkBlue,
+            Background = ConsoleColor.DarkBlue
+        };
+        readonly Color ActiveEmptyColor = new Color()
+        {
+            Foreground = ConsoleColor.DarkGray,
+            Background = ConsoleColor.Black
+        };
+        readonly Color InactiveEmptyColor = new Color()
+        {
+            Foreground = ConsoleColor.Black,
+            Background = ConsoleColor.Black
+        };
+        readonly Color ActiveBorderColor = new Color()
+        {
+            Foreground = ConsoleColor.Gray,
+            Background = ConsoleColor.Black
+        };
+        readonly Color InactiveBorderColor = new Color()
+        {
+            Foreground = ConsoleColor.Gray,
+            Background = ConsoleColor.Black
+        };
+
+        #endregion // Constants
+        
+        #region Fields & Properties
+
+        public enum Type
+	    {
+	        Empty,
+            Cross,
+            Nought
+	    }
+
+        Type[,] field = new Type[3,3];
+        Point currentPoint;
+        Type turn;
+
+        #endregion // Fields & Properties
+
+        #region Events
+        
+        public delegate void TurnChangeHandler(TicTacToeField field, Type newTurn, Type oldTurn);
+        public event TurnChangeHandler OnTurnChanged;
+
+        public delegate void GameOverHandler(TicTacToeField field, Type winner);
+        public event GameOverHandler OnGameOver;
+
+        #endregion // Events
+
+        public TicTacToeField(UIElement parent, Point pos) :
+            base(parent, new Rectangle(pos, new Size( 4 + 3*3, 4 + 2*3 )))
+        {
+            PrepareField();
+        }
+
+        private void PrepareField()
+        {
+            currentPoint = new Point(1,1);
+            for(int i = 0; i < 3; ++i)
+                for(int j = 0; j < 3; ++j)
+                    field[i,j] = Type.Empty;
+            turn = (new Random()).NextDouble() > 0.5 ? Type.Cross : Type.Nought;
+            Emit(OnTurnChanged, this, turn, Type.Empty);
+        }
+        
+        // Handles:
+        // * KeyPressedMessage
+        public override void Handle(IMessage msg)
+        {
+            if(msg is KeyPressedMessage)
+            {
+                var keyInfo = (msg as KeyPressedMessage).KeyInfo;
+                if( keyInfo.EqualsTo(GoLeftKeyInfo) )
+                {
+                    if( currentPoint.X > 0 )
+                    {
+                        currentPoint.X--;
+                        Invalidate();
+                    }
+                }
+                else if( keyInfo.EqualsTo(GoRightKeyInfo) )
+                {
+                    if( currentPoint.X < 2 )
+                    {
+                        currentPoint.X++;
+                        Invalidate();
+                    }
+                }
+                else if( keyInfo.EqualsTo(GoUpKeyInfo) )
+                {
+                    if( currentPoint.Y > 0 )
+                    {
+                        currentPoint.Y--;
+                        Invalidate();
+                    }
+                }
+                else if( keyInfo.EqualsTo(GoDownKeyInfo) )
+                {
+                    if( currentPoint.Y < 2 )
+                    {
+                        currentPoint.Y++;
+                        Invalidate();
+                    }
+                }
+                else if( keyInfo.EqualsTo(PutMarkKeyInfo) )
+                {
+                    if( field[currentPoint.X, currentPoint.Y] == Type.Empty )
+                    {
+                        field[currentPoint.X, currentPoint.Y] = turn;
+                        
+                        if( ! CheckGameOver() )
+                        {
+                            var oldTurn = turn;
+                            turn = (oldTurn == Type.Cross) ? Type.Nought : Type.Cross;
+                            Emit(OnTurnChanged, this, turn, oldTurn);
+                        }
+                        Invalidate();
+                    }
+                }
+            }
+            return;
+        }
+
+        public override void Draw(DrawSpace ds)
+        {
+            // border
+            ds.Color = IsActive() ? ActiveBorderColor : InactiveBorderColor;
+            // Characters: ┌ ┐ ┘ └ ─ │ ─ │ ┬ ┤ ┴ ├ ┼
+            ds.DrawBorder(new Rectangle(0,0,5,4), "┌┬┼├─│─│");
+            ds.DrawBorder(new Rectangle(8,0,5,4), "┬┐┤┼─│─│");
+            ds.DrawBorder(new Rectangle(8,6,5,4), "┼┤┘┴─│─│");
+            ds.DrawBorder(new Rectangle(0,6,5,4), "├┼┴└─│─│");
+            
+            ds.DrawLine(new Point(4+1,0), new Point(8-1,0), '─');
+            ds.DrawLine(new Point(4+1,3), new Point(8-1,3), '─');
+            ds.DrawLine(new Point(4+1,6), new Point(8-1,6), '─');
+            ds.DrawLine(new Point(4+1,9), new Point(8-1,9), '─');
+            
+            ds.DrawLine(new Point(0,3+1), new Point(0,6-1), '│');
+            ds.DrawLine(new Point(4,3+1), new Point(4,6-1), '│');
+            ds.DrawLine(new Point(8,3+1), new Point(8,6-1), '│');
+            ds.DrawLine(new Point(12,3+1), new Point(12,6-1), '│');
+
+            // noughts & crosses
+            for(int i = 0; i < 3; ++i)
+                for(int j = 0; j < 3; ++j)
+                {
+                    var pos = new Point(1+4*i, 1+3*j);
+                    switch (field[i,j])
+                    {
+                        case Type.Empty:
+                            ds.Color = IsActive() ? ActiveEmptyColor : InactiveEmptyColor;
+                            break;
+                        case Type.Cross:
+                            ds.Color = IsActive() ? ActiveCrossColor : InactiveCrossColor;
+                            break;
+                        case Type.Nought:
+                            ds.Color = IsActive() ? ActiveNoughtColor : InactiveNoughtColor;
+                            break;
+                        default:
+                            break;
+                    }
+                    var ch = (currentPoint == new Point(i,j)) ? '░' : ' ';
+                    ds.DrawLine(pos + new Size(0,0), pos + new Size(2,0), ch);
+                    ds.DrawLine(pos + new Size(0,1), pos + new Size(2,1), ch);
+                }
+        }
+
+        private bool CheckGameOver()
+        {
+            Type winner = Type.Empty;
+            for(int i = 0; i < 3; ++i)
+            {
+                if(field[i,0] == field[i,1] && field[i,1] == field[i,2] && field[i,1] != Type.Empty)
+                    winner = field[i,1];
+                if(field[0,i] == field[1,i] && field[1,i] == field[2,i] && field[1,i] != Type.Empty)
+                    winner = field[1,i];
+            }
+            if(field[0,0] == field[1,1] && field[1,1] == field[2,2] && field[1,1] != Type.Empty)
+                winner = field[1,1];
+            if(field[2,0] == field[1,1] && field[1,1] == field[0,2] && field[1,1] != Type.Empty)
+                winner = field[1,1];
+
+            if(winner != Type.Empty)
+            {
+                Emit(OnGameOver, this, winner);
+                PrepareField();
+                return true;
+            }
+            return false;
+        }
+    }
+}
